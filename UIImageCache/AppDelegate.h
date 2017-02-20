546e24707555375a2c85052c20a4193023d26269
blackmatch <blackmatch@blackmatchs-mac-mini.local>
2017-02-20 12:47:13 +0800

    Initial Commit

---
+++ UIImageCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImageCache
+//
+//  Created by blackmatch on 2017/2/20.
+//  Copyright © 2017年 blackmatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIImageCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImageCache
+//
+//  Created by blackmatch on 2017/2/20.
+//  Copyright © 2017年 blackmatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
